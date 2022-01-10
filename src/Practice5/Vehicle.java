59ecd2f0a404a1ab410c6c58b3d5879e3e3b6c3d
s-nagi9343 <s.nagisa9343@gmail.com>
2022-01-11 00:57:26 +0900

    Java Lessons Complete

---
+++ src/Practice5/Vehicle.java
@@ -0,0 +1,49 @@
+package Practice5;
+
+abstract class Vehicle {
+    private String name;
+    private String color;
+    protected int distance = 0;
+    private Person owner;
+
+    Vehicle(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+
+    public Person getOwner() {
+        return this.owner;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public void printData() {
+        System.out.println("名前：" + this.name);
+        System.out.println("色：" + this.color);
+        System.out.println("走行距離：" + this.distance);
+    }
+
+    abstract public void run(int distance);
+}
